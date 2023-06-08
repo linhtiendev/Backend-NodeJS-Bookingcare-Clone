f96acb79e7b54b83258c03c6acd2370f3864313f
linhtiendev <lelinhtien0707@gmail.com>
2023-06-08 16:34:05 +0700

    setup project MVC

--- src/router/web.js
+++ src/router/web.js
@@ -1,11 +1,13 @@
 import express from "express";
+import homeController from "../controllers/homeController";
 
 let router = express.Router();
 
 let initWebRoutes = (app) => {
-    router.get("/", (req, res) => {
-        return res.send("Hello world!");
-    });
+    router.get("/", homeController.getHomePage);
+    router.get("/aboutpage", homeController.getAboutPage);
+    // rest api
+
     return app.use("/", router);
 };
 module.exports = initWebRoutes;
