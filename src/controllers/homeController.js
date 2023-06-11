9e5ca7bf36255ffeeb658579fc2cd223104761a0
linhtiendev <lelinhtien0707@gmail.com>
2023-06-11 18:52:28 +0700

    use the ORM to create a new database (CREATE)

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,5 @@
 import db from "../models/index";
-
+import CRUDService from "../services/CRUDService";
 let getHomePage = async (req, res) => {
     try {
         let data = await db.User.findAll();
@@ -15,11 +15,18 @@ let getAboutPage = (req, res) => {
     return res.render("test/aboutpage.ejs");
 };
 
-// object: {
-//     key:'',
-//     value:''
-// }
+let getCRUD = (req, res) => {
+    return res.render("crud.ejs");
+};
+
+let postCRUD = async (req, res) => {
+    let message = await CRUDService.createNewUser(req.body);
+    console.log(message);
+    return res.send("post crud form server");
+};
 module.exports = {
     getHomePage: getHomePage,
     getAboutPage: getAboutPage,
+    getCRUD: getCRUD,
+    postCRUD: postCRUD,
 };
--- src/router/web.js
+++ src/router/web.js
@@ -6,6 +6,9 @@ let router = express.Router();
 let initWebRoutes = (app) => {
     router.get("/", homeController.getHomePage);
     router.get("/aboutpage", homeController.getAboutPage);
+    router.get("/crud", homeController.getCRUD);
+
+    router.post("/post-crud", homeController.postCRUD);
     // rest api
 
     return app.use("/", router);
