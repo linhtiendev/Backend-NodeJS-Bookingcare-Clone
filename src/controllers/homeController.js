976063bc63f22b3908c1ee31f6bc5dafec1df177
linhtiendev <lelinhtien0707@gmail.com>
2023-06-12 21:23:32 +0700

    use the ORM to edit data (EDIT)

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -36,10 +36,36 @@ let displayGetCRUD = async (req, res) => {
         dataTable: data,
     });
 };
+
+let getEditCRUD = async (req, res) => {
+    let userId = req.query.id;
+    // lay thong tin nguoi dung thong qua id
+    if (userId) {
+        let userData = await CRUDService.getUserInfoById(userId);
+        // check userData not found
+        // let user data
+        return res.render("editCRUD.ejs", {
+            user: userData,
+        });
+    } else {
+        return res.send("User not found");
+    }
+};
+// function lay input tu put-crud
+let putCRUD = async (req, res) => {
+    let data = req.body;
+    let allUsers = await CRUDService.updateUserData(data);
+    return res.render("displayCRUD.ejs", {
+        dataTable: allUsers,
+    });
+};
+
 module.exports = {
     getHomePage: getHomePage,
     getAboutPage: getAboutPage,
     getCRUD: getCRUD,
     postCRUD: postCRUD,
     displayGetCRUD: displayGetCRUD,
+    getEditCRUD: getEditCRUD,
+    putCRUD: putCRUD,
 };
